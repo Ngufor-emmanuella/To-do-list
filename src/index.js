41bc61b75db22d4db3ec5d4844013bfc5ef29845
Ngufor-emmanuella <98039632+Ngufor-emmanuella@users.noreply.github.com>
2022-10-15 22:37:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { addTask, clear, store } from './functions.js';
 
 const form = document.querySelector('form');
 let todo;
+const inputEl = document.querySelector('.input');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (inputEl.value !== '') {
