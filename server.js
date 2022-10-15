923e4aee935b91b14c3b94d58e5d462321b2d568
Ngufor-emmanuella <98039632+Ngufor-emmanuella@users.noreply.github.com>
2022-10-15 21:10:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,1 @@
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
-
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
 
