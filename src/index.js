1fa505caf63f2480da8517a44c378f213aa6177c
Ngufor-emmanuella <98039632+Ngufor-emmanuella@users.noreply.github.com>
2022-10-15 22:59:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import './style.css';
+import { clear } from './clearEl.js';
+/* eslint-disable */
+import { del, trueFalse } from './interraction.js';
+/* eslint-enable */
 
 const form = document.querySelector('form');
-const inputEl = document.querySelector('.input');
+const input = document.querySelector('.input');
 const todoContainer = document.querySelector('.todoContainer');
-
 let todo;
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+/* eslint-disable */
+export let todos = JSON.parse(localStorage.getItem('todos')) || [];
+/* eslint-enable */
 const store = () => {
   todo = {
-    Description: inputEl.value,
+    Description: input.value,
     id: todos.length,
     completed: false,
   };
   todos.push(todo);
   localStorage.setItem('todos', JSON.stringify(todos));
-};
-
-const clear = () => {
-  inputEl.value = '';
 };
 
 const removeBook = (id) => {
@@ -30,33 +31,69 @@ const removeBook = (id) => {
 
 const addTask = (todo) => {
   const ul = document.createElement('div');
+
   const checkBox = document.createElement('input');
   checkBox.type = 'checkbox';
+  checkBox.value = todo.completed;
+  checkBox.checked = todo.completed;
   checkBox.classList.add('checkBox');
+
   const newInp = document.createElement('input');
   newInp.type = 'text';
   newInp.classList.add('newInput');
   newInp.value = todo.Description;
+  if (checkBox.checked) {
+    newInp.style.textDecoration = 'line-through';
+    newInp.style.opacity = '0.5';
+  }
   const icon = document.createElement('i');
   icon.classList.add('fa-solid');
   icon.classList.add('fa-ellipsis-vertical');
   icon.classList.add('dots');
+
   const hr = document.createElement('hr');
+
   ul.append(checkBox, newInp, icon, hr);
+
   todoContainer.append(ul);
-  icon.addEventListener('click', () => {
-    icon.parentElement.remove();
-    removeBook(todo.id);
+
+  newInp.addEventListener('focusin', () => {
+    icon.classList.add('fa-trash');
+    icon.classList.add('bin');
+    icon.classList.remove('drag');
+    icon.classList.remove('fa-ellipsis-vertical');
+    icon.addEventListener('click', () => {
+      icon.parentElement.remove();
+      removeBook(todo.id);
+    });
   });
+  newInp.addEventListener('focusout', () => {
+    icon.classList.remove('fa-trash');
+    icon.classList.add('fa-ellipsis-vertical');
+    icon.classList.add('drag');
+  });
+  trueFalse();
 };
-todos.forEach(addTask);
+Array.prototype.forEach.call(todos, addTask);
+
+const clearCompleted = document.querySelector('#clear-completed');
+
+clearCompleted.addEventListener('click', () => {
+  const filterAll = todos.filter((item) => item.completed !== true);
+  todos = filterAll
+  todos.forEach((todo, id) => {
+    todo.id = id;
+  });
+  localStorage.setItem('todos', JSON.stringify(todos));
+  del();
+});
 
 const editTodoList = () => {
   const editInput = document.querySelectorAll('.newInput');
-  editInput.forEach((edits, indexy) => {
+  editInput.forEach((edits, indexed) => {
     edits.addEventListener('change', () => {
       todos.forEach((todo, index) => {
-        if (indexy === index) {
+        if (indexed === index) {
           todo.Description = edits.value;
           localStorage.setItem('todos', JSON.stringify(todos));
         }
@@ -68,7 +105,7 @@ editTodoList();
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (inputEl.value !== '') {
+  if (input.value !== '') {
     store();
     addTask(todo);
     clear();
