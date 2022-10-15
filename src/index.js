2f58cf0125549fad04c94e1dbed2661992431493
Ngufor-emmanuella <98039632+Ngufor-emmanuella@users.noreply.github.com>
2022-10-15 22:34:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import { addTask, clear, store } from './functions.js'
+import { addTask, clear, store } from './functions.js';
 
 const form = document.querySelector('form');
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (inputEl.value !== '') {
